be2ddff053b0f176f5ee06fc4f2fca5c9a6eefd2
Carlos Mosquera <camosqueraba@gmail.com>
2021-03-10 12:13:51 -0500

    creando migraciones

---
+++ database/migrations/2021_03_10_162430_create_anuncios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnunciosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anuncios', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('titulo');
+            $table->text('contenido');
+            $table->string('enlace');
+            $table->string('imagen');
+            $table->unsignedInteger('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anuncios');
+    }
+}
